fdbbb0133d2604bef0f3adc3abc0af6a3d39789a
Kat <xkat0341@gmail.com>
2017-11-28 14:47:16 +0100

    Bijtelling Calculator

---
+++ Bijtelling Calculator/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bijtelling_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int belastingschijf = 41;
+            int catalogusPrice = Convert.ToInt32(textBox1.Text);
+            int bijtellingKlasse = Convert.ToInt32(textBox2.Text);
+            int totaleJaarBedrag = catalogusPrice / 100 * bijtellingKlasse;
+            int maandBedrag = totaleJaarBedrag / 100 * belastingschijf / 12;
+            textBox3.Text = maandBedrag.ToString();
+        }
+    }
+}
